cb208265c1b278931cb8892514efc5296749283b
amiroslaw <arek47@gmail.com>
2023-10-22 00:22:08 +0200

    fix sorting by last changed, refactoring

--- lua/telescope/_extensions/changes.lua
+++ lua/telescope/_extensions/changes.lua
@@ -5,50 +5,39 @@ local actions = require 'telescope.actions'
 local actions_state = require 'telescope.actions.state'
 local actions_set = require 'telescope.actions.set'
 
-local function prepare_output_table(last_changes)
-	local bufnr = vim.api.nvim_get_current_buf()
-	local filepath = vim.api.nvim_buf_get_name(bufnr)
-
-	local output_table = {}
-	for _, val in pairs(last_changes) do
-		table.insert(output_table, {
-			value = val.nr,
-			display = val.display,
-			ordinal = val.display,
-			lnum = val.row,
-			col = val.col,
-			filename = filepath,
-			bufnr = bufnr,
-		})
-	end
-	return output_table
+-- TODO maybe remove empty lines  and '-invalid-'
+local function reverse(tab)
+    for i = 1, #tab/2, 1 do
+        tab[i], tab[#tab-i+1] = tab[#tab-i+1], tab[i]
+    end
+    return tab
 end
 
-function get_changes()
+local function get_display(line, row)
+	-- return vim.trim(line)
+	return row .. ': ' .. line:sub(18)
+end
+
+local function get_changes()
 	local last_changes = {}
 	local changes = vim.api.nvim_command_output 'changes'
 
 	for change in changes:gmatch '[^\r\n]+' do
-		local i = 1
-		local nr, row, col
-		for match in string.gmatch(change, '([^%s]+)') do
-			if i == 1 then
-				nr = match
-			elseif i == 2 and tonumber(match) then
-				row = match
-			elseif i == 3 then
-				col = match
-			end
-			i = i + 1
-		end
+		local match = change:gmatch('%d+')
+		local nr = match()
+		local row = match()
+		local col = match()
+
 		if row then
-			last_changes[row] = { row = row, nr = nr, col = col, display = vim.trim(change) }
+			table.insert(last_changes, { row = row, nr = nr, col = col, display = get_display(change, row) })
 		end
 	end
-	return prepare_output_table(last_changes)
+	return reverse(last_changes)
 end
 
 local function show_changes(opts)
+	local bufnr = vim.api.nvim_get_current_buf()
+	local filepath = vim.api.nvim_buf_get_name(bufnr)
 	opts = opts or {}
 	pickers.new(opts, {
 			prompt_title = 'Changes',
@@ -56,19 +45,21 @@ local function show_changes(opts)
 				results = get_changes(),
 				entry_maker = function(entry)
 					return {
-						value = entry.value,
+						value = entry,
 						display = entry.display,
 						ordinal = entry.display,
-						lnum = tonumber(entry.lnum),
+						-- ordinal = tostring(entry.nr),
+						lnum = tonumber(entry.row),
 						col = tonumber(entry.col),
-						filename = entry.filename,
-						bufnr = entry.bufnr,
+						filename = filepath,
+						bufnr = bufnr,
 					}
 				end,
 			},
 			previewer = conf.grep_previewer(opts),
 			-- previewer = conf.qflist_previewer(opts),
 			sorter = conf.generic_sorter(opts),
+			-- sorting_strategy = "ascending",
 			attach_mappings = function(prompt_bufnr)
 				actions_set.select:replace(function()
 					local entry = actions_state.get_selected_entry()
